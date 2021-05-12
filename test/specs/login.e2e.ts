39d583ed73a61794e45df92dfbed9e381b2446c4
Taras <taras.qa.automation@gmail.com>
2021-05-12 19:50:44 +0300

    Initial setup commit

---
+++ test/specs/login.e2e.ts
@@ -0,0 +1,15 @@
+import LoginPage from  '../pageobjects/login.page';
+import SecurePage from '../pageobjects/secure.page';
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeDefined();
+        await expect(SecurePage.flashAlert).toContain(
+            'You logged into a secure area!');
+    });
+});
+
+
